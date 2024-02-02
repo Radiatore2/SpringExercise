3e6e41f7a9cd5786b74a5390dffa73153e087f68
Radiatore2 <radiatore2@gmail.com>
2024-02-02 16:25:20 +0100

    EX1 Spring

---
+++ ex1spring/src/test/java/com/ex1spring/ex1spring/Ex1springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ex1spring.ex1spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
