f1443b0ab8be5db45df151ba326f5720a3b2597d
Radiatore2 <radiatore2@gmail.com>
2024-03-05 14:36:14 +0100

    Ex1 Interceptor

---
+++ Ex1Interceptor/src/main/java/com/example/Ex1Interceptor/Ex1InterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ex1Interceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex1InterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex1InterceptorApplication.class, args);
+	}
+
+}
