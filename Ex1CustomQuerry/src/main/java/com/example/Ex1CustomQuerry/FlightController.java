29dfddb90887ce40b239749bfeef5a40fbd7a7ac
Radiatore2 <radiatore2@gmail.com>
2024-03-07 19:26:49 +0100

    Ex1 Custom

--- Ex1CustomQuerry/src/main/java/com/example/Ex1CustomQuerry/FlightController.java
+++ Ex1CustomQuerry/src/main/java/com/example/Ex1CustomQuerry/FlightController.java
@@ -1,7 +1,7 @@
 package com.example.Ex1CustomQuerry;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +17,7 @@ public class FlightController {
 
 	private int numFlights = 50;
 
-	@GetMapping("/provision")
+	@PostMapping("/provision")
 	public void provisionFlight(){
 		Random random = new Random();
 
