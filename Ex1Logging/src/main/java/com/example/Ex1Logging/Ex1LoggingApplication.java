5e43bb6520efbf6a21c12c4b01c6b3c84951be4e
Radiatore2 <radiatore2@gmail.com>
2024-03-26 17:09:06 +0100

    xrdcfvg

---
+++ Ex1Logging/src/main/java/com/example/Ex1Logging/Ex1LoggingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ex1Logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex1LoggingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex1LoggingApplication.class, args);
+	}
+
+}
---
+++ Ex1Logging/src/test/java/com/example/Ex1Logging/Ex1LoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ex1Logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1LoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
