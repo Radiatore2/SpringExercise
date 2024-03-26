5ed32aa243ea40ff405d6b443f99dd0013a70404
Radiatore2 <radiatore2@gmail.com>
2024-03-26 16:31:27 +0100

    cgvhb

---
+++ Ex1Environment/src/main/java/com/example/Ex1Environment/Ex1EnvironmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ex1Environment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex1EnvironmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex1EnvironmentApplication.class, args);
+	}
+
+}
---
+++ Ex1Environment/src/test/java/com/example/Ex1Environment/Ex1EnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ex1Environment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex1EnvironmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
