a8ed96a3ca1712d792f4b6abe93b6078de2fee35
Radiatore2 <radiatore2@gmail.com>
2024-03-26 16:39:00 +0100

    zfdxgf

---
+++ Ex2Environment/src/main/java/com/example/Ex2Environment/Ex2EnvironmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ex2Environment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex2EnvironmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex2EnvironmentApplication.class, args);
+	}
+
+}
---
+++ Ex2Environment/src/test/java/com/example/Ex2Environment/Ex2EnvironmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Ex2Environment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex2EnvironmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
