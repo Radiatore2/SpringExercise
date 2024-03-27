6c72689e37353342869479b749240a16fad573fc
Radiatore2 <radiatore2@gmail.com>
2024-03-27 11:17:50 +0100

    fvghbjn

---
+++ Ex1Swagger/src/main/java/com/example/Ex1Swagger/Ex1SwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Ex1Swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex1SwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ex1SwaggerApplication.class, args);
+	}
+
+}
